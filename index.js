c891f83ebc696e3a7ff3e1825aac72769165e7d0
vanita22 <inesvanessapcm@gmail.com>
2021-01-30 22:32:02 -0300

    cargando formulario

--- index.js
+++ index.js
@@ -1,14 +1,62 @@
 let http = require('http');
 let fs = require('fs');
+let path = require('path');
+let mime = require('mime');
 
 http.createServer((request, response)=>{
 
-    function condicional (content) {
+    /*function condicional (content) {
         response.write(content); 
         response.end();
-    }
+    }*/
 
-    if (request.url === "/") {
+    //response.setHeader("Content-Type", "text/html; charset=utf-8");
+    if (request.method === "GET") {
+        switch (request.url) {
+            case "/":
+                readFile("/index.html", response);
+                break;
+            case "/nosotros":
+                readFile("/about.html", response);
+                break;
+            case "/proyectos":
+                readFile("/projects.html", response);
+                break;
+            case "/contacto":
+                readFile("/contact.html", response);
+                break;
+            case "/favicon.ico":
+                response.setHeader("Content-Type", "image/x-icon");
+                readFile("/favicon.ico", response);
+                break;
+            default:
+                //response.statusCode = 404;
+                readFile(request.url, response);
+                break;
+        }
+    } else if (request.method === "POST") {
+        let data = '';
+        request.on('data', chunk => {
+            data += chunk;
+            console.log(chunk);
+        })
+        request.on('end', () => {
+            const usuario = data.toString();
+            console.log("fin del stream");
+            fs.appendFile("usuarios.txt", usuario + "\n", (error) => {
+                if (error) {
+                    console.log(error);
+                }
+                response.end("se ha escrito en el usuario.txt");
+            });
+        })
+        request.on('error', error => {
+            console.log(error);
+        })
+    }
+    
+
+    /*if (request.url === "/") {
         response.setHeader("Content-Type", "text/html; charset=utf-8");
         fs.readFile("./index.html", (error, content) => {
             if (!error) {
@@ -48,5 +96,27 @@ http.createServer((request, response)=>{
         fs.readFile("./404.html", (error, content) => {            
             condicional(content);            
         })
-    }    
+    }*/    
 }).listen(8080);
+
+const readFile = (url, response) => {
+    let urlF = __dirname + url;
+    fs.readFile( urlF, (error, content) => {
+        if (!error) {
+            //setContentType(path.extname(urlF), response);
+            response.setHeader("Content-Type", mime.getType(urlF));
+            response.end(content);
+        } else {
+            response.writeHead(404); 
+            response.end("<h1>404</h1>");
+        }
+    })
+}
+
+/*const setContentType = (ext, response) => {
+    if (ext === ".css") {
+        response.setHeader("Content-Type", "text/css");
+    } else if (ext === ".html") {
+        response.setHeader("Content-Type", "text/html");
+    }
+}*/
